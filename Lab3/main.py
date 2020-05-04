d48d72cb426dbdbd6ea9e4417a636337fdff7d43
Denys Shutka <shine.at.dusk@gmail.com>
2020-05-04 19:44:41 +0300

    fix: corrected misspells

--- Lab3/main.py
+++ Lab3/main.py
@@ -2,25 +2,25 @@ def main():
     number = 729
     print(f'Binary form of {number} is {bin(number)}')
 
-    print('\tBinary -> Grey')
-    grey = binary_to_grey(number)
-    print(f'Number {bin(number)} converted to Grey code: {bin(grey)}')
+    print('\tBinary -> Gray')
+    gray = binary_to_gray(number)
+    print(f'Number {bin(number)} converted to Gray code: {bin(gray)}')
 
-    print('\tGrey -> Binary')
-    binary = grey_to_binary(grey)
-    print(f'Number in gray code {bin(number)} converted to binary: {bin(binary)}')
+    print('\tGray -> Binary')
+    binary = gray_to_binary(gray)
+    print(f'Number in Gray code {bin(number)} converted to binary: {bin(binary)}')
 
 
-def binary_to_grey(number):
-    """Convert binary to Grey code"""
+def binary_to_gray(number):
+    """Convert binary to Gray code"""
     if isinstance(number, str):
         number = int(number, 2)
 
     return number ^ (number >> 1)
 
 
-def grey_to_binary(number):
-    """Convert Grey code to binary"""
+def gray_to_binary(number):
+    """Convert Gray code to binary"""
     if isinstance(number, str):
         number = int(number, 2)
 
@@ -33,3 +33,4 @@ def grey_to_binary(number):
 
 if __name__ == '__main__':
     main()
+
